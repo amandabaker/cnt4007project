2e30bd8a41125c895f1c1410946f9c05d5f338db
Amanda Baker <amandaabaker21@gmail.com>
2016-10-20 21:30:27 -0400

    Clean up handshake

--- Client.java
+++ Client.java
@@ -9,11 +9,22 @@ import java.lang.*;
 import java.math.BigInteger;
 
 public class Client {
-	Socket requestSocket;           //socket connect to the server
-	ObjectOutputStream out;         //stream write to the socket
- 	ObjectInputStream in;          //stream read from the socket
-	String message;                //message send to the server
-	byte[] MESSAGE;                //capitalized message read from the server
+
+	final int CHOKE 			= 0; 
+	final int UNCHOKE 			= 1;
+	final int INTERESTED 		= 2;
+	final int NOT_INTERESTED 	= 3;
+	final int HAVE 				= 4;
+	final int BITFIELD 			= 5;
+	final int REQUEST 			= 6;
+	final int PIECE 			= 7;
+
+	Socket requestSocket;          	//socket connect to the server
+	ObjectOutputStream out;        	//stream write to the socket
+	OutputStream outStream;
+ 	ObjectInputStream in;          	//stream read from the socket
+	String message;                	//message send to the server
+	byte[] MESSAGE;                	//capitalized message read from the server
 
 	public void Client() {}
 
@@ -80,58 +91,42 @@ public class Client {
 			}
 		}
 	}
+
 	//send a message to the output stream
-	void sendMessage(byte[] msg)
+	void sendMessage(Message msg)
 	{
 		try{
-			//stream write the message
-			out.writeObject(msg);
-			out.flush();
+			msg.send(requestSocket);
 		}
 		catch(IOException ioException){
 			ioException.printStackTrace();
 		}
 	}
+
 	//handshake method
 	void handshake(String peerid)
 	{
-		//add peer id to end of handshake message
-		String hmsg = "P2PFILESHARINGPROJ0000000000" + peerid;
-		
-		//for debugging purposes
-		//System.out.println("peerid: " + peerid);
-		//System.out.println("hmsg: " + hmsg);
-		//hmsg = "12";
-		//translate from string to byte
-		byte [] b = hmsg.getBytes();
-		
-		//System.out.println("header bytes: " + b);
-		
-
-		//System.out.println("header bytes: " + Integer.toBinaryString(b));
-		
-		//translate from byte to string
-		String bytestring = new String(b, StandardCharsets.UTF_8);
-		System.out.println("stringfrombytes: " + bytestring);
-
-		//send handshake message
-		String msg = hmsg;
 		try{
 			//stream write the message
-			out.writeObject(b);
-			out.flush();
+			outStream.write((byte)"P2PFILESHARINGJPROJ");
+			outStream.write((byte)0);
+			outStream.write((byte)peerid);
+		
+			outStream.flush();
 		}
 		catch(IOException ioException){
 			ioException.printStackTrace();
 		}
 	}
+
 	void handshakeCheck(String hcheck) 
 	{
 		//check whether handshake was successful and connected peer is valid
 	}
+	
 	int messageLength(byte[] ml) 
 	{
-		byte[] meslen = new byte[3];
+		byte[] meslen = new byte[4];
 		for (int i = 0; i<5; i++) 
 		{
 			meslen[i] = ml[i]; 
@@ -140,6 +135,7 @@ public class Client {
 		return new BigInteger(meslen).intValue();
 		
 	}
+
 	void messageType(byte [] message) 
 	{
 		//check message type bit
@@ -178,6 +174,7 @@ public class Client {
 			//piece
 		}
 	}
+
 	//main method
 	public static void main(String args[])
 	{
